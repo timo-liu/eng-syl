7bc94d5cf9a0381044bb092bf9e9a4890992fdf9
ellipse-liu <timothys.new.email@gmail.com>
2024-07-31 08:56:06 -0700

    Revert "Merge pull request #6 from anowa-eng/main"
    This reverts commit 5389f4a295cfff5921d2b35925e628b82447ce45, reversing
    changes made to f29c958b3ae9e8a08959d12516fa5c49227aae0b.

--- eng_syl/syllabify.py
+++ eng_syl/syllabify.py
@@ -1,4 +1,4 @@
-from tensorflow.keras.layers import Dense, TimeDistributed, Bidirectional, Input, Embedding, Activation, GRU, ZeroPadding1D
+from tensorflow.keras.layers import  Dense, TimeDistributed, Bidirectional, Input, Embedding, Activation, GRU, ZeroPadding1D
 from tensorflow.keras.models import Model
 from tensorflow.keras import regularizers
 from tensorflow.keras.preprocessing.sequence import pad_sequences
@@ -6,63 +6,75 @@ import pickle
 import numpy as np
 import os
 
-class Syllable:
-    def __init__(self, e2i_ortho='e2i.pkl', ortho_input_size=45, latent_dim=256, embed_dim=256, max_feat=259):
-        self.this_dir = os.path.dirname(os.path.abspath(__file__))
+
+class Syllabel:
+    def __init__(self,e2i_ortho ='e2i.pkl', ortho_input_size=45, latent_dim=256, embed_dim=256, max_feat=259):
+        self.this_dir, this_filename = os.path.split(__file__)
         path_clean = os.path.join(self.this_dir, 'clean.pkl')
-        with open(path_clean, 'rb') as f:
-            self.clean = pickle.load(f)
+        self.clean = pickle.load(open(path_clean,'rb'))
         path_e2i = os.path.join(self.this_dir, e2i_ortho)
-        with open(path_e2i, 'rb') as f:
-            self.e2i_ortho = pickle.load(f)
+        self.e2i_ortho = pickle.load(open(path_e2i, 'rb'))
         self.ortho_input_size = ortho_input_size
         self.latent_dim = latent_dim
         self.embed_dim = embed_dim
         self.max_feat = max_feat
         self.model = self.build_model()
-        self.model.load_weights(os.path.join(self.this_dir, 'syllabler_best_weights.h5'))
+        self.model.load_weights(os.path.join(self.this_dir,'syllabler_best_weights.h5'))
+
 
     def build_model(self):
-        # orthographic input layer
-        ortho_inputs = Input(shape=(self.ortho_input_size,))
-        # embedding layer
-        x = Embedding(input_dim=self.max_feat, output_dim=self.embed_dim)(ortho_inputs)
-        # bidirectional GRU layers
-        x = Bidirectional(GRU(units=self.latent_dim, return_sequences=True, recurrent_dropout=0.2, activity_regularizer=regularizers.l2(1e-5)))(x)
-        x = Bidirectional(GRU(units=self.latent_dim, return_sequences=True, recurrent_dropout=0.2, activity_regularizer=regularizers.l2(1e-5)))(x)
-        # time distributed dense layer
-        x = TimeDistributed(Dense(units=3))(x)
-        # activation layer
-        z = Activation('softmax')(x)
-        # define the model
+        
+        # orthographic and ipa input layers
+        ortho_inputs = Input(self.ortho_input_size,)
+        # first branch ortho
+        x = Embedding(self.max_feat, self.embed_dim, input_length=self.ortho_input_size)(ortho_inputs)
+        x = Bidirectional(GRU(self.latent_dim, return_sequences=True, recurrent_dropout = 0.2, activity_regularizer=regularizers.l2(1e-5)), input_shape=(self.ortho_input_size, 1))(x)
+        x = Bidirectional(GRU(self.latent_dim, return_sequences=True, recurrent_dropout = 0.2, activity_regularizer= regularizers.l2(1e-5) ), input_shape=(self.ortho_input_size, 1))(x)
+        # x = TimeDistributed(Dense(3, activation = 'softmax'))(x)
+        x = ZeroPadding1D(padding=(0, self.ortho_input_size - x.shape[1]))(x)
+        z = TimeDistributed(Dense(3))(x)
+        z = Activation('softmax')(z)
+        
         model = Model(inputs=[ortho_inputs], outputs=z)
+        
         return model
-
+    
     def syllabify(self, word):
         if word in self.clean:
             return self.clean[word]
-        inted_ortho = [self.e2i_ortho[c] for c in word.lower()]
+        inted_ortho = []
+        for c in word.lower():
+            inted_ortho += [self.e2i_ortho[c]]
+            
+        
         inted_ortho = pad_sequences([inted_ortho], maxlen=self.ortho_input_size, padding='post')[0]
-        predicted = self.model.predict(inted_ortho.reshape(1, self.ortho_input_size), verbose=0)[0]
+        predicted = self.model.predict(inted_ortho.reshape(1, self.ortho_input_size, 1), verbose = 0)[0]
         indexes = self.to_ind(predicted)
         converted = self.insert_syl(word, indexes)
         return converted
 
     def machine_syllabify(self, word):
-        inted_ortho = [self.e2i_ortho[c] for c in word.lower()]
+        inted_ortho = []
+        for c in word.lower():
+            inted_ortho += [self.e2i_ortho[c]]
+
+
         inted_ortho = pad_sequences([inted_ortho], maxlen=self.ortho_input_size, padding='post')[0]
-        predicted = self.model.predict(inted_ortho.reshape(1, self.ortho_input_size), verbose=0)[0]
+        predicted = self.model.predict(inted_ortho.reshape(1, self.ortho_input_size, 1), verbose = 0)[0]
         indexes = self.to_ind(predicted)
         converted = self.insert_syl(word, indexes)
         return converted
 
     def to_ind(self, sequence):
-        index_sequence = [np.argmax(ind) for ind in sequence]
+        index_sequence = []
+        for ind in sequence:
+            index_sequence += [np.argmax(ind)]
         return index_sequence
-
+    
     def insert_syl(self, word, indexes):
         index_list = np.where(np.array(indexes) == 2)[0]
-        word_array = list(word)
-        for i in range(len(index_list)):
+        word_array = [*word]
+        for i in range(0, len(index_list)):
             word_array.insert(index_list[i] + i + 1, '-')
         return ''.join(word_array)
+
